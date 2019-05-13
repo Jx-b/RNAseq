3becb1e9c93a019663a7230359de41d293e0d5f0
Jb <Jxb.dev@gmail.com>
2019-05-13 18:05:35 +0200

    reorganised directory

--- pipelines/tools/utilities.py
+++ pipelines/tools/utilities.py
@@ -3,7 +3,7 @@
 import os, sys
 import ftplib, requests, shlex
 import subprocess as sp
-import progressbar as pg
+import tools.progressbar as pg
 
 def download_file(url, local_filename):
     with open(local_filename, 'wb') as f:
